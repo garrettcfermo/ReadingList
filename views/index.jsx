385add7cbfd0df5c7372a16675715ad2219f5cf3
Garrett Fermo <garrettcfermo@gmail.com>
2018-10-24 23:10:17 -0700

    Created Home Page

---
+++ views/index.jsx
@@ -0,0 +1,20 @@
+const React = require('react')
+
+const Page = () =>
+  <html lang="en">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>Reading List</title>
+
+      <!-- Styles -->
+      <link rel="stylesheet" href="../public/CSS/reset.css" />
+
+    </head>
+    <body>
+      <h1>Hello</h1>
+    </body>
+  </html>
+
+module.exports = Page
