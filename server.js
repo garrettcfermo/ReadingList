e9b9a5b4fcc6a5c005ec263d387beaf1c7250eee
Garrett Fermo <garrettcfermo@gmail.com>
2018-10-24 23:03:16 -0700

    Server JS Setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const bodyparser = require('body-parser')
+const PORT = process.env.PORT || 3000
+const app = express()
+
+// Middleware
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }))
+
+// HTML/API Routes
+
+
+
+
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
